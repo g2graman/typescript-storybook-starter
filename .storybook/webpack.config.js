b524d5620c5646b3ee872ad73448d8ec70e8a7db
Francesco <fran.gramano@mail.utoronto.ca>
2018-04-22 16:17:23 -0400

    Updates a bunch of dependencies

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,15 @@
 // load the default config generator.
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 
-module.exports = (baseConfig, env) => {
-    const config = genDefaultConfig(baseConfig, env);
-
+module.exports = (baseConfig, env, defaultWebpackConfig) => {
     // Extend it as you need.
 
     // For example, add typescript loader:
-    config.module.rules.push({
+    defaultWebpackConfig.module.rules.push({
         test: /\.(ts|tsx)$/,
-        loader: require.resolve('awesome-typescript-loader')
+        loader: require.resolve('ts-loader')
     });
-    config.resolve.extensions.push('.ts', '.tsx');
 
-    return config;
+    defaultWebpackConfig.resolve.extensions.push('.ts', '.tsx');
+
+    return defaultWebpackConfig;
 };
